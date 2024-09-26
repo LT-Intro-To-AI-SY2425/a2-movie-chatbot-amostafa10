2322174206a80998311a5902aa9785809e5d52fa
Ali Mostafa <amostafa1@cps.edu>
2024-09-26 11:29:06 -0500

    translated solution to python

--- a2.py
+++ a2.py
@@ -1,28 +1,44 @@
 from typing import List
 
+def safeIndex(list, index):
+    return list[index] if len(list) > index else None
 
 def match(pattern: List[str], source: List[str]) -> List[str]:
-    patternIndex = 0
-    sourceIndex = 0
+    source.append("")
+    output = []
 
-    while True:
-        patternWord = pattern[patternIndex]
-        sourceWord = source[sourceIndex]
+    for patternIndex, patternString in enumerate(pattern):
+        if len(source) == 0:
+            return None
 
-        
-        
+        percentMatchResult = ""
+        percentMatchLength = 0
+        nextPatternString = safeIndex(pattern, patternIndex + 1)
 
-        if patternWord == sourceWord:
-            continue
+        for sourceIndex, sourceString in enumerate(source):
+            if patternString == "%":
+                if sourceString == nextPatternString:
+                    output.append(percentMatchResult.strip())
+                    source = source[percentMatchLength:]
+                    break
+                else:
+                    if sourceString != "":
+                        percentMatchResult += sourceString + " "
+                        percentMatchLength += 1
+                    if sourceIndex == len(source) - 1:
+                        output.append(percentMatchResult.strip())
+                        source = source[percentMatchLength:]
+            elif patternString == "_":
+                output.append(sourceString)
+                del source[sourceIndex]
+                break
+            else:
+                if sourceString == patternString:
+                    del source[sourceIndex]
+                    break
+                return None
 
-        
-
-        patternIndex += 1
-        sourceIndex += 1
-
-
-
-    return result
+    return output if safeIndex(source, 0) == "" else None
 
 
 
@@ -41,14 +57,7 @@ if __name__ == "__main__":
     assert match(["x", "%", "y", "z"], ["x", "y", "z"]) == [""], "test 11 failed"
     assert match(["x", "y", "z", "%"], ["x", "y", "z"]) == [""], "test 12 failed"
     assert match(["_", "%"], ["x", "y", "z"]) == ["x", "y z"], "test 13 failed"
-    assert match(["_", "_", "_", "%"], ["x", "y", "z"]) == [
-        "x",
-        "y",
-        "z",
-        "",
-    ], "test 14 failed"
-    # this last case is a strange one, but it exposes an issue with the way we've
-    # written our match function
+    assert match(["_", "_", "_", "%"], ["x", "y", "z"]) == [ "x", "y", "z", "",], "test 14 failed"
     assert match(["x", "%", "z"], ["x", "y", "z", "z", "z"]) == None, "test 15 failed"
 
     print("All tests passed!")
